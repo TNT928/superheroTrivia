381ad992d9863d640bf1057cf347c1c854bc0050
Samantha Stutzman <pepper3art@gmail.com>
2020-02-26 19:51:43 -0500

    main page done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+
+import { RouterModule, Routes } from '@angular/router';
+
+const AppRoutes: Routes = [
+  // { path: 'marvel', component: marvel-component },
+  // { path: 'dc', component: dc-component },
+  // { path: 'comicsvsmovies', component: comics-vs-movies-component }
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(AppRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]

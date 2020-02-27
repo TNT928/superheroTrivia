ccd30882c1c7d8d11067ce08ab0bd7151778650d
Samantha Stutzman <pepper3art@gmail.com>
2020-02-26 21:08:24 -0500

    adding routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,27 @@
+import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { MarvelComponent } from './marvel/marvel.component';
+import { MoviesVsComicsComponent } from './movies-vs-comics/movies-vs-comics.component';
+import { MainPageComponent } from './main-page/main-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  { path: 'marvel', component: MarvelComponent },
+  // { path: 'dc', component: dc-component },
+  { path: 'comics-vs-movies', component: MoviesVsComicsComponent },
+  { path: '', component: MainPageComponent }
+  
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  declarations: [
+  ],
+  imports: [
+    RouterModule.forRoot(routes),
+    BrowserModule,
+  ],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule, Component } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { MarvelComponent } from './marvel/marvel.component';
-import { HttpClientModule } from '@angular/common/http';
 import { MoviesVsComicsComponent } from './movies-vs-comics/movies-vs-comics.component';
 import { MainPageComponent } from './main-page/main-page.component';
-
-import { RouterModule, Routes } from '@angular/router';
-
-const AppRoutes: Routes = [
-  { path: 'marvel', component: MarvelComponent },
-  { path: 'dc', component: dc-component },
-  { path: 'comicsvsmovies', component: MoviesVsComicsComponent },
-  { path: '/', component: MainPageComponent }
-]
 
 @NgModule({
   declarations: [
     AppComponent,
 
-    MarvelComponent
+    MarvelComponent,
     MoviesVsComicsComponent,
     MainPageComponent
   ],
@@ -29,7 +21,6 @@ const AppRoutes: Routes = [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule
-    RouterModule.forRoot(AppRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]

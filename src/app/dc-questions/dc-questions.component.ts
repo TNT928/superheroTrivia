0d4349259e3aa3181ef2d3ef9af114a05206e5ab
Michael Vanderloon <jenkins92886@gmail.com>
2020-02-26 20:43:20 -0500

    more stuff

--- src/app/dc-questions/dc-questions.component.ts
+++ src/app/dc-questions/dc-questions.component.ts
@@ -1,62 +1,52 @@
-import { Component, OnInit } from '@angular/core';
-import{ Question,Answer} from '../../../models/DCquestions'
+import { Component, OnInit } from "@angular/core";
+import { Question, Answer } from "../../../models/DCquestions";
 
 @Component({
-  selector: 'app-dc-questions',
-  templateUrl: './dc-questions.component.html',
-  styleUrls: ['./dc-questions.component.css']
+  selector: "app-dc-questions",
+  templateUrl: "./dc-questions.component.html",
+  styleUrls: ["./dc-questions.component.css"]
 })
 export class DcQuestionsComponent implements OnInit {
-  questions: Question[]
-  question1: Answer[]
-  question2: Answer[]
-  question3: Answer[]
-  clicked: boolean =false
+  questions: Question[];
+  question1: Answer[];
+  question2: Answer[];
+  question3: Answer[];
+  clicked: boolean = false;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-    this.questions =[
+    this.questions = [
+      {
+        question: "Who is Batman",
+        answers: [
+          { correct: true, answer: "Bruce Wayne" },
+          { correct: false, answer: "Clark Kent" },
+          { correct: false, answer: "Slade Wilson" },
+          { correct: false, answer: "Dick Grayson" }
+        ]
+      },
+      {
+        question:
+          "What is the name of the commisoner who has befriended Batman?",
+          answers: [
+            { correct: false, answer: "Carl Bishops" },
+            { correct: false, answer: "Harvey Bullock" },
+            { correct: false, answer: "Tim Drake" },
+            { correct: true, answer: "Commisoner Gordon" }
+          ]
+      },
+      { question: "Which Batman villian is actually a vantrilquest dummy?",
+    answers:  [
+      { correct: false, answer: "The Riddler" },
+      { correct: false, answer: "Clayface" },
+      { correct: false, answer: "King Shark" },
+      { correct: true, answer: "Scarface" }
+    ]}
+    ];
+  }
 
-      {question:'Who is Batman'},
-      {question:'What is the name of the commisoner who has befriended Batman?'},
-      {question: 'Which Batman villian is actually a vantrilquest dummy?'}
- ]
-    
-
-   
-      this.question1 = [
-       
-        {correct: true,answer: 'Bruce Wayne' },
-        {correct:false, answer: 'Clark Kent'},
-        {correct: false, answer: 'Slade Wilson'},
-        {correct: false, answer: 'Dick Grayson'}
-      ]
-
-      this.question2= [
-        
-        {correct: false, answer: 'Carl Bishops'},
-        {correct: false, answer: 'Harvey Bullock'},
-        {correct: false, answer: 'Tim Drake'},
-        {correct: true, answer: 'Commisoner Gordon'}
-      ],
-
-      this.question3=[
-       
-        {correct: false, answer: 'The Riddler'},
-        {correct: false, answer: 'Clayface'},
-        {correct: false, answer: 'King Shark'},
-        {correct: true, answer:'Scarface'}
-      ]
-    
-     
-
-    }
-
-    askQuestion(){
-      this.clicked=  true
-     
-    }
-  
-
+  askQuestion() {
+    this.clicked = true;
+  }
 }

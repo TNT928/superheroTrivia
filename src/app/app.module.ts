7da99e454c4722b91b1e1577d10e473dd98440b9
JO15on <JOlson1530@hotmail.com>
2020-02-26 20:07:23 -0500

    merge conflict

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,9 @@ import { NgModule, Component } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
-<<<<<<< HEAD
 import { MarvelComponent } from './marvel/marvel.component';
 import { HttpClientModule } from '@angular/common/http';
-
-=======
-=======
 import { MoviesVsComicsComponent } from './movies-vs-comics/movies-vs-comics.component';
->>>>>>> 67580f355803c0fd8d4fc40cded0618aab980914
 import { MainPageComponent } from './main-page/main-page.component';
 
 import { RouterModule, Routes } from '@angular/router';
@@ -21,29 +15,20 @@ const AppRoutes: Routes = [
   // { path: 'dc', component: dc-component },
   // { path: 'comicsvsmovies', component: comics-vs-movies-component }
 ]
->>>>>>> f553d1fa0a81bf53ce0488645b7430bcac1da3a4
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-<<<<<<< HEAD
+
     MarvelComponent
-=======
-=======
     MoviesVsComicsComponent,
->>>>>>> 67580f355803c0fd8d4fc40cded0618aab980914
     MainPageComponent
->>>>>>> f553d1fa0a81bf53ce0488645b7430bcac1da3a4
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-<<<<<<< HEAD
     HttpClientModule
-=======
     RouterModule.forRoot(AppRoutes),
->>>>>>> f553d1fa0a81bf53ce0488645b7430bcac1da3a4
   ],
   providers: [],
   bootstrap: [AppComponent]

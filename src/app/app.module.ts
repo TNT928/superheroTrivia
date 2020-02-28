1fc5d6fb2eff8259177115934cfbdaff46bcafa7
Michael Vanderloon <jenkins92886@gmail.com>
2020-02-27 21:11:38 -0500

    formatting

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,25 +3,18 @@ import { NgModule, Component } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
-
+import {FormsModule} from '@angular/forms'
 import { AppComponent } from './app.component';
 import { DcQuestionsComponent } from './dc-questions/dc-questions.component';
 import { MarvelComponent } from './marvel/marvel.component';
 import { MoviesVsComicsComponent } from './movies-vs-comics/movies-vs-comics.component';
 import { MainPageComponent } from './main-page/main-page.component';
 import { RouterModule, Routes } from '@angular/router';
-<<<<<<< HEAD
-import {FormsModule} from '@angular/forms';
-const AppRoutes: Routes = [
-  { path: 'marvel', component: MarvelComponent },
-//  { path: 'dc', component: dc-component },
-=======
 import { AnswersComponent } from './answers/answers.component';
 
 const AppRoutes: Routes = [
   { path: 'marvel', component: MarvelComponent },
   { path: 'dc', component: DcQuestionsComponent },
->>>>>>> bec3d7e6752b92b1a799073e3b067c6ca413476d
   { path: 'comicsvsmovies', component: MoviesVsComicsComponent },
   { path: '', component: MainPageComponent }
 ]
--- src/app/dc-questions/dc-questions.component.ts
+++ src/app/dc-questions/dc-questions.component.ts
@@ -7,71 +7,73 @@ import { Question, Answer } from "../../../models/DCquestions";
   styleUrls: ["./dc-questions.component.css"]
 })
 export class DcQuestionsComponent implements OnInit {
-  questions: Question[];
-  // question1: Answer[];
-  // question2: Answer[];
-  // question3: Answer[];
+  // questions: Question[];
+  questions = [
+    {
+      question: "Who is Batman",
+      answers: [
+        { correct: true, answer: "Bruce Wayne" },
+        { correct: false, answer: "Clark Kent" },
+        { correct: false, answer: "Slade Wilson" },
+        { correct: false, answer: "Dick Grayson" }
+      ]
+    },
+    {
+      question: "What is the name of the commisoner who has befriended Batman?",
+      answers: [
+        { correct: false, answer: "Carl Bishops" },
+        { correct: false, answer: "Harvey Bullock" },
+        { correct: false, answer: "Tim Drake"},
+        { correct: true, answer: "Commisoner Gordon" }
+      ]
+    },
+    {
+      question: "Which Batman villian is actually a vantrilquest dummy?",
+      answers: [
+        { correct: false, answer: "The Riddler" },
+        { correct: false, answer: "Clayface" },
+        { correct: false, answer: "King Shark" },
+        { correct: true, answer: "Scarface" }
+      ]
+    },
+    {
+      question: "Who causes Batgirl to become Oracle",
+      answers: [
+        { correct: false, answer: "Two-Face" },
+        { correct: false, answer: "Harley Quinn" },
+        { correct: true, answer: "The Joker" },
+        { correct: false, answer: "Bane" }
+      ]
+    },
+    {
+      question: "What was the Jokers orginal crime persona?",
+      answers: [
+        { correct: true, answer: "The Red Hood" },
+        { correct: false, answer: "Jester Man" },
+        { correct: false, answer: "Arkham Knight" },
+        { correct: false, answer: "The Riddler" }
+      ]
+    }
+  ];
+
   clicked: boolean = false;
+  question: any;
 
   constructor() {}
 
   ngOnInit() {
-    this.questions = [
-      {
-        question: "Who is Batman",
-        answers: [
-          { correct: true, answer: "Bruce Wayne" },
-          { correct: false, answer: "Clark Kent" },
-          { correct: false, answer: "Slade Wilson" },
-          { correct: false, answer: "Dick Grayson" }
-        ]
-      },
-      {
-        question:
-          "What is the name of the commisoner who has befriended Batman?",
-          answers: [
-            { correct: false, answer: "Carl Bishops" },
-            { correct: false, answer: "Harvey Bullock" },
-            { correct: false, answer: "Tim Drake" },
-            { correct: true, answer: "Commisoner Gordon" }
-          ]
-      },
-      { question: "Which Batman villian is actually a vantrilquest dummy?",
-    answers:  [
-      { correct: false, answer: "The Riddler" },
-      { correct: false, answer: "Clayface" },
-      { correct: false, answer: "King Shark" },
-      { correct: true, answer: "Scarface" }
-    ]},
-      { question: "Who causes Batgirl to become Oracle",
-    answers:  [
-      { correct: false, answer: "Two-Face" },
-      { correct: false, answer: "Harley Quinn" },
-      { correct: true, answer: "The Joker" },
-      { correct: false, answer: "Bane" }
-    ]},
-      { question: "What was the Jokers orginal crime persona?",
-    answers:  [
-      { correct: true, answer: "The Red Hood" },
-      { correct: false, answer: "Jester Man" },
-      { correct: false, answer: "Arkham Knight" },
-      { correct: false, answer: "The Riddler" }
-    ]}
-    ];
+    this.next();
   }
 
   askQuestion() {
     this.clicked = true;
   }
 
-onClick(question){
-  console.log(question.correct);
+  onClick(question) {
+    console.log(question.correct);
   }
 
-
-
+  next() {
+    [Math.floor(Math.random() * Math.floor(this.questions.length))];
+  }
 }
-
-
-
-  
